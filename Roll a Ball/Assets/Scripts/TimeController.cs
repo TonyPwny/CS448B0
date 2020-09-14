f9f597b62762074946313c6503b6cf4db299b4dd
Anthony Tiongson <anthonytiongson@me.com>
2020-09-14 15:39:15 -0400

    updated list of resources used

--- Roll a Ball/Assets/Scripts/TimeController.cs
+++ Roll a Ball/Assets/Scripts/TimeController.cs
@@ -1,8 +1,13 @@
 ﻿// Anthony Tiongson (ast119)
-// https://learn.unity.com/project/roll-a-ball-tutorial
+// List of resources used:
 // https://answers.unity.com/questions/1417767/c-how-to-check-if-player-is-grounded.html
-// https://answers.unity.com/questions/1275232/disable-all-inputs.html
 // https://answers.unity.com/questions/1261937/creating-a-restart-button.html
+// https://answers.unity.com/questions/1275232/disable-all-inputs.html
+// https://answers.unity.com/questions/1400361/how-to-create-a-border-around-2d-text.html
+// https://answers.unity.com/questions/885849/how-to-respawn-with-a-simple-delay.html
+// https://gamedev.stackexchange.com/questions/151670/unity-how-to-detect-collision-occuring-on-child-object-from-a-parent-script
+// https://learn.unity.com/tutorial/fps-mod-customize-the-sky
+// https://learn.unity.com/project/roll-a-ball-tutorial
 // https://www.youtube.com/watch?v=qc7J0iei3BU
 
 using System;
